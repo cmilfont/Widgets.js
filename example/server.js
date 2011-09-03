f5c74e0c41785191f03f264bf127dcea8dd5960a
heronmedeiros <heron.medeiros@gmail.com>
2011-09-03 10:55:45 -0300

    fix names

--- example/server.js
+++ example/server.js
@@ -5,8 +5,8 @@ app.set('views', __dirname + '/views');
 app.register('.html', require('ejs'));
 app.set('view engine', 'html');
 
-app.use(express.bodyDecoder());
-app.use(express.cookieDecoder());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
 app.use(express.session({ secret: 'milfont' }));
 
 app.use(app.router);
@@ -36,6 +36,6 @@ app.get('/oportunidades:format', function(req, res){
 
 
 app.use(express.errorHandler({ showStack: true }));
-app.use(express.staticProvider(__dirname));
+app.use(express.static(__dirname));
 app.listen(8001);
 

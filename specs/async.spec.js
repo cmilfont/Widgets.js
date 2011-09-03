20249595a992cb1bcc41ce83f3864c7645def0c5
Christiano Milfont <cmilfont@gmail.com>
2011-09-03 18:39:05 -0300

    teste assincrono

---
+++ specs/async.spec.js
@@ -0,0 +1,55 @@
+describe("Modal Async", function(){
+    
+    var spriteLoader, json, parsed, $modal;
+    
+    beforeEach(function() {
+       var fs = require("fs");
+       parsed = fs.readFileSync(__dirname + "/parsed.html").toString();
+       global.Eljs = require("eljs").Eljs;
+
+       jsonTemplate = {
+                "Widgets": {
+                    Modal: {
+                        head: {
+                            title: "Teste de Modal"
+                        },
+                        body: "Teste do corpo"
+                    },
+                    buttons: [
+                        {
+                            name: "Butao"
+                        }
+                    ]
+                }
+            };
+       
+        jQuery.extend({
+            get: function( url, callback ) {
+                var templatePath = __dirname + "/" + url;
+                fs.readFile(templatePath, function (err, data) {
+                  if (err) throw err;
+                  callback(data.toString());
+                });
+            }
+        });
+       require('../lib/widgets.dialog');
+       var url = "template.html";
+       $modal = jQuery("<div>").modal(jsonTemplate, url);
+    });
+    
+    it("Deveria renderizar o template de forma async", function() {
+
+        waitsFor(function(){
+            return $modal.html().toString() !== "";
+        }, "Rendered Template", 10000);
+        runs(function(){
+        	var rendered = $modal.html().toString();
+            expect(parsed).toEqual(rendered);
+        });
+        
+    });
+    
+
+    
+    
+})
